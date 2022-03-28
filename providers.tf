e326cf0027d5ac2b5ed35fd7c6c326c5e489c8a3
Erickson Assis do Nascimento <enascimento87@hotmail.com>
2022-03-28 03:19:52 -0300

    Tentando ajustar pipeline

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  project = "lab-devops-cloud-6aso"
+  project = "lab-devops-cloud-6aso-345503"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
